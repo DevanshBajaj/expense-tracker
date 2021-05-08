48d518fb24d6305a2d935c0cdb4200916e5ebbc3
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-08 18:43:55 +0530

    add a component with basic jsx for test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>Let's Get Started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
