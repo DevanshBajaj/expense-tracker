cb02ef80fc97a433c117e82fd6676aa79efdec9d
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-12 17:48:55 +0530

    Restructured files and using arrow function now

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,8 +1,8 @@
 import './ExpenseItem.css';
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date}/>
