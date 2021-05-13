376d97b31b64fb162686502a6946edb246cb2b14
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-14 05:02:18 +0530

    Added filter component and working with events

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 	const [enteredTitle, setEnteredTitle] = useState("");
 	const [enteredAmount, setEnteredAmount] = useState("");
 	const [enteredDate, setEnteredDate] = useState("");
@@ -20,12 +20,13 @@ const ExpenseForm = () => {
 	const submitHandler = (event) => {
 		event.preventDefault();
 
-		const expenseDate = {
+		const expenseData = {
 			title: enteredTitle,
 			amount: enteredAmount,
 			date: new Date(enteredDate),
 		};
-		console.log(expenseDate);
+		
+        props.onSaveExpenseData(expenseData);
 		setEnteredTitle("");
 		setEnteredAmount("");
 		setEnteredDate("");
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,19 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
-    return <div className="new-expense">
-        <ExpenseForm />
-    </div>
+const NewExpense = (props) => {
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(expenseData);
+    };
+    return (
+        <div className="new-expense">
+            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+        </div>
+    );
 };
 
 export default NewExpense
