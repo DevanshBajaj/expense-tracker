54732266c8ef345830d2b95cbe04a0972f4f4389
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-13 18:37:15 +0530

    using useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,15 @@
+import React, { useState } from 'react';
+
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
 
-	let title = props.title;
+	const [title, setTitle] = useState(props.title);
 
 	const clickHandler = () => {
-		title = 'Updated';
+		setTitle('Updated!');
 		console.log(title);
 	};
 
