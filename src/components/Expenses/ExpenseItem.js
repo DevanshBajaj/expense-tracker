586fae0e4f6b4241c7f64373973e113b9efbcfa5
Devansh Bajaj <devansh21db.db@gmail.com>
2021-05-15 23:56:01 +0530

    Removed unnecessary button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpenseItem.css';
 import Card from '../UI/Card';
@@ -6,21 +6,13 @@ import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
 
-	const [title, setTitle] = useState(props.title);
-	console.log('Evaluated')
-	const clickHandler = () => {
-		setTitle('Updated!');
-		console.log(title);
-	};
-
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date}/>
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
-			<button onClick={clickHandler}>Change title</button>
 		</Card>
 	);
 }
